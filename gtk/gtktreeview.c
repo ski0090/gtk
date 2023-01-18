ce1d3acb920e238bb18d84a18bafe5ee2415db31
powerory <powerory@atins.io>
2023-01-18 15:50:19 +0900

    gtktreeview: allow themes to draw treeview rows in alternating colurs
    patch from https://github.com/lah7/gtk3-classic/pull/42

--- gtk/gtktreeview.c
+++ gtk/gtktreeview.c
@@ -5303,6 +5303,14 @@ gtk_tree_view_bin_draw (GtkWidget      *widget,
 
           gtk_style_context_add_class (context, GTK_STYLE_CLASS_CELL);
 
+    if (_gtk_rbtree_node_get_index (tree, node) % 2 == 0)
+      gtk_style_context_add_class (context, "odd");
+    else
+      gtk_style_context_add_class (context, "even");
+
+    if (flags & GTK_CELL_RENDERER_SORTED)
+      gtk_style_context_add_class (context, "sorted");
+
 	  if (node == tree_view->priv->cursor_node && has_can_focus_cell
               && ((column == tree_view->priv->focus_column
                    && tree_view->priv->draw_keyfocus &&
